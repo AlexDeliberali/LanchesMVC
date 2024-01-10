f46dd46b4c72c73875c53071c083f21173ec363e
alexsander.santos <alexsander.santos@Not_GAtec>
2024-01-10 10:33:46 -0300

    Adicionar arquivos de projeto.

---
+++ LanchesMVC/Startup.cs
@@ -0,0 +1,39 @@
+﻿namespace LanchesMVC;
+
+public class Startup
+{
+
+    public Startup(IConfiguration configuration)
+    {
+        configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints => { endpoints.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}"); });
+    }
+}
