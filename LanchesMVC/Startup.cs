a91e472b66111807f92590013e548567ed377d47
Alex Deliberali <alexdeliberali@gmail.com>
2024-01-22 12:23:47 -0300

    Ajustes para realizar o migration corretamente

--- LanchesMVC/Startup.cs
+++ LanchesMVC/Startup.cs
@@ -8,14 +8,14 @@ public class Startup
 
     public Startup(IConfiguration configuration)
     {
-        configuration = configuration;
+        Configuration = configuration;
     }
 
     public IConfiguration Configuration { get; }
 
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddDbContext<AppDbContext>(options =>
+        services.AddDbContext<AppDbContext>(options => 
             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         services.AddControllersWithViews();
     }
