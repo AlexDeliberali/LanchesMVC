c394477d068fee1d9ff3d73e29f49bd866f022c8
Alex Deliberali <alexdeliberali@gmail.com>
2024-01-22 17:17:59 -0300

    Criando o LancheControler e a View list

---
+++ LanchesMVC/Controllers/LancheController.cs
@@ -0,0 +1,21 @@
+﻿using LanchesMVC.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMVC.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
