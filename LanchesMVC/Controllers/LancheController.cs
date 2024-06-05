75c68761b42612b3c0868520911b1a346a13849d
alexsander.santos <alexsander.santos@Not_GAtec>
2024-06-05 11:25:47 -0300

    Ajuste na conexão com o banco de dados e no Lanche Controller

--- LanchesMVC/Controllers/LancheController.cs
+++ LanchesMVC/Controllers/LancheController.cs
@@ -1,14 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LanchesMVC.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMVC.Controllers
 {
     public class LancheController : Controller
     {
 
+        private readonly ILancheRepository _lancheRepository;
+        public LancheController(ILancheRepository lancheRepository) { 
+            _lancheRepository = lancheRepository;
+        }
 
         public IActionResult List()
         {
-            return View();
+            //Obtendo uma lista de lanches
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
         }
     }
 }
