0483ae0ed816c8087bbd3e42ad79ca2821346d46
alexsander.santos <alexsander.santos@Not_GAtec>
2024-06-17 17:30:02 -0300

    Adicionado o tagHelpers e a aba COntato com envio de email

--- LanchesMVC/Controllers/LancheController.cs
+++ LanchesMVC/Controllers/LancheController.cs
@@ -1,4 +1,5 @@
-﻿using LanchesMVC.Repositories.Interfaces;
+﻿using LanchesMVC.Models;
+using LanchesMVC.Repositories.Interfaces;
 using LanchesMVC.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,15 +13,42 @@ namespace LanchesMVC.Controllers
             _lancheRepository = lancheRepository;
         }
 
-        public IActionResult List()
+        public IActionResult List(string categoria)
         {
             //var lanches = _lancheRepository.Lanches;
             //return View(lanches);
 
             //Criando uma instância da ViewModel
-            var lanchesListViewModel = new LancheListViewModel();
-            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
-            lanchesListViewModel.CategoriaAtual = "Categoria Atual";
+            //var lanchesListViewModel = new LancheListViewModel();
+            //lanchesListViewModel.Lanches = _lancheRepository.Lanches;
+            //lanchesListViewModel.CategoriaAtual = "Categoria Atual";
+
+            IEnumerable<Lanche> lanches;
+            string categoriaAtual = string.Empty;
+
+            if (string.IsNullOrEmpty(categoria))
+            {
+                lanches = _lancheRepository.Lanches.OrderBy(l => l.LancheId);
+                categoriaAtual = "Todos os lanches";
+            }
+            else
+            {
+                if(string.Equals("Normal",categoria,StringComparison.OrdinalIgnoreCase))
+                {
+                    lanches = _lancheRepository.Lanches.Where(l => l.Categoria.CategoriaNome.Equals("Normal")).OrderBy(l => l.Nome);
+                } else
+                {
+                    lanches = _lancheRepository.Lanches.Where(l => l.Categoria.CategoriaNome.Equals("Natural")).OrderBy(l => l.Nome);
+                }
+                categoriaAtual = categoria;
+            }
+
+            var lanchesListViewModel = new LancheListViewModel
+            {
+                Lanches = lanches,
+                CategoriaAtual = categoriaAtual
+            };
+
             return View(lanchesListViewModel);
         }
     }
