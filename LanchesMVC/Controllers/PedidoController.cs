cbef8b860836da5eaa2948fa6935f82549e15de6
Alexsander Deliberali <deliberali.alexsander@outlook.com>
2024-08-08 22:51:51 -0300

    Adição da tela de checkout do pedido e ajuste na tabela Pedidos

--- LanchesMVC/Controllers/PedidoController.cs
+++ LanchesMVC/Controllers/PedidoController.cs
@@ -26,7 +26,48 @@ namespace LanchesMVC.Controllers
         [HttpPost]
         public IActionResult Checkout(Pedido pedido)
         {
-            return View();
+            int totalItensPedido = 0;
+            decimal precoTotalPedido = 0.0m;
+
+            //Obter os itens do carrinho de compra do cliente
+            List<CarrinhoCompraItem> items = _carrinhoCompra.GetCarrinhoCompraItems();
+            _carrinhoCompra.CarrinhoCompraItems = items;
+
+            //Verificar se existem itens de pedido
+            if (_carrinhoCompra.CarrinhoCompraItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho está vazio!");
+            }
+
+            //Calcular o total de itens e o preco total do pedido
+            foreach(var item in items)
+            {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += (item.Lanche.Preco * item.Quantidade);
+            }
+
+            //Atribuindo os valores ao pedido
+            pedido.TotalItensPedido = totalItensPedido;
+            pedido.PedidoTotal = precoTotalPedido;
+
+            //Validar os dados do pedido
+            //Se cair no ModelError = fica com estado inválido
+            if(ModelState.IsValid)
+            {
+                //Criar o pedido e os itens do pedido
+                _pedidoRepository.CriarPedido(pedido);
+
+                //Definir mensagens ao cliente
+                ViewBag.CheckoutCompletoMsg = "Obrigado pelo pedido!";
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+                //Limpar o carrinho pois conclui o pedido
+                _carrinhoCompra.LimparCarrinho();
+
+                //Exibe a view com dados do cliente e do pedido
+                return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+            }
+            return View(pedido);
         }
     }
 }
---
+++ LanchesMVC/Migrations/20240809014650_CorrecaoBanco.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMVC.Migrations
+{
+    public partial class CorrecaoBanco : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Sobremome",
+                table: "Pedidos",
+                newName: "Sobrenome");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Cidade",
+                table: "Pedidos",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Estado",
+                table: "Pedidos",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cidade",
+                table: "Pedidos");
+
+            migrationBuilder.DropColumn(
+                name: "Estado",
+                table: "Pedidos");
+
+            migrationBuilder.RenameColumn(
+                name: "Sobrenome",
+                table: "Pedidos",
+                newName: "Sobremome");
+        }
+    }
+}
