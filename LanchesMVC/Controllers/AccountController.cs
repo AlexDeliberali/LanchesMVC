095a116afdd9af6e47ad7b51e7080769e63f2a8f
Alexsander Deliberali <deliberali.alexsander@outlook.com>
2024-08-09 20:23:42 -0300

    Adição da tela de cadastro e login, e validações de usuário

---
+++ LanchesMVC/Controllers/AccountController.cs
@@ -0,0 +1,80 @@
+﻿using LanchesMVC.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if (!ModelState.IsValid) return View(loginVM);
+
+            //Usando uma instância de userManager para tentar localizar o usuário pelo nome
+            var user = await _userManager.FindByNameAsync(loginVM.UserName);
+
+            if (user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                //Verificando se o login foi realizado com sucesso
+                if (result.Succeeded)
+                {
+                    //Verifica se o valor de retorno é null ou vazio, se for, redireciona para a página principal
+                    if (string.IsNullOrEmpty(loginVM.ReturnUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    // Se não for, retorna para a página que ele estava antes do login
+                    return Redirect(loginVM.ReturnUrl);
+                }
+            }
+            ModelState.AddModelError("", "Falha ao realizar o login no sistema!");
+            return View(loginVM);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //Adicionando a validação para proteção de clonagem de permissão para acesso ao site (Ataque CSRF)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(LoginViewModel registerVM)
+        {
+            if (!ModelState.IsValid) {
+                var user = new IdentityUser { UserName = registerVM.UserName };
+                var result = await _userManager.CreateAsync(user, registerVM.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    this.ModelState.AddModelError("Regsitro", "Falha ao inserir o usuário.");
+                }
+            }
+            return View(registerVM);
+        }
+    }
+}
---
+++ LanchesMVC/ViewModels/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LanchesMVC.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Informe o usuário")]
+        [Display(Name = "Usuário")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha")]
+        //Permite que o campo no formulário é exibido os caracteres de senha
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+
+    }
+}
